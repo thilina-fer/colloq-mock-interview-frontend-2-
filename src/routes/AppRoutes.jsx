6db0ce2cf87c4182fc2832a2238738cce8816d1f
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-12 09:16:02 +0530

    feat: add route for CandidateDashboard component

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import AuthView from "../components/landingpage/AuthView";
 import LandingPage from "../pages/LandingPage";
+import CandidateDashboard from "../pages/CandidateDashboard";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/auth" element={<AuthView />} />
+      <Route path="/candidate" element={<CandidateDashboard />} />
     </Routes>
   </BrowserRouter>
 );
